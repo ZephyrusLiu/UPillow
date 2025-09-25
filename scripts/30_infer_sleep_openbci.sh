97b8caa33f5dd81531cce71cefa836e9a5f89f22
Yuqi Liu <yuqliu@umass.edu>
2025-09-25 10:52:44 -0400

    Update

--- scripts/30_infer_sleep_openbci.sh
+++ scripts/30_infer_sleep_openbci.sh
@@ -1,1 +1,4 @@
-python -m src.sleep.infer_sleep_openbci --input OpenBCI-RAW-2024-09-11_09-43-10.txt
+python -m src.sleep.infer_sleep_openbci \
+  --input OpenBCI-RAW-2024-09-11_09-43-10.txt \
+  --virtual-map configs/openbci_virtual_fpz_pzoz.json \
+  --disable-car
